5cc983c4bd2428335f8c5ebf453ae72161fbe6f3
Wei Tang <tangwei@smail.nju.edu.cn>
2025-04-02 15:25:39 +0200

    minor

--- src/toolbox/gauge_fixing.jl
+++ src/toolbox/gauge_fixing.jl
@@ -9,7 +9,7 @@ their transfer matrix. Returns the unitary transformation and convergence metric
 - `AL2`: Target MPS tensor to be gauge-fixed (will be transformed to match AL1's gauge)
 
 # Returns
-- `U::AbstractTensorMap`: Unitary transformation matrix satisfying ``AL1 ≈ U' * AL2 * U``
+- `U::AbstractTensorMap`: Unitary transformation matrix satisfying ``AL1 ≈ U * AL2 * U' ``
 - `conv_meas::Real`: if AL1 and AL2 are equivalent up to a unitary transformation, this should be 0. This is a measure of the deviation between the MPS's represented by AL1 and AL2
 """
 function gauge_fixing(AL1::AbstractTensorMap, AL2::AbstractTensorMap)
